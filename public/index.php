49227d506ca494679f419d2bd902742abf4a1178
spiritabsolute <spiritabsolute@yandex.ru>
2018-01-25 18:00:22 +0200

    Structure and operation with HTTP

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Hello!";
+use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\ServerRequestFactory;
+use Zend\Diactoros\Response\SapiEmitter;
+
+require __DIR__."/../vendor/autoload.php";
+
+### Initialization
+$request = ServerRequestFactory::fromGlobals();
+
+### Action
+$name = $request->getQueryParams()["name"] ?? "Guest";
+
+$response = (new HtmlResponse("Hello, ".$name."!"))
+	->withHeader("X-Developer", ["Spirit"]);
+
+### Sending
+
+$emitter = new SapiEmitter();
+$emitter->emit($response);
